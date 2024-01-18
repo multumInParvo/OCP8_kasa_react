56cc80204c3f79879229e2aec157f76143374b4d
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-18 03:52:29 +0100

    removed unnecessary elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 // src/components/Footer.js
 import React from 'react';
+import logo from '../assets/logofooter.png';
 
 function Footer() {
   return (
     <footer>
-      {/* footer content goes here */}
-      <p>&copy; 2024 My App. All rights reserved.</p>
+      <img src={logo} alt="Logo footer" />
+      <p>© 2024 Kasa. All rights reserved</p>
     </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
 // Header.js
-
 import React from 'react';
+import logo from '../assets/logoheader.png';
 
 const Header = () => {
   return (
     <header>
-        <h1>My Awesome Website</h1>
       <div>
-        <img src="path/to/logo.png" alt="Logo" />
+        <img src={logo} alt="Logo header" />
       </div>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          {/* Add more navigation links as needed */}
+          <li><a href="/">Accueil</a></li>
+          <li><a href="/about">A Propos</a></li>
+          {/* to add navigation links as needed */}
         </ul>
       </nav>
     </header>
