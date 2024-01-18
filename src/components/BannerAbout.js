d828befb8d2550d76af757eae0354544172ea40b
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-18 12:06:12 +0100

    created banner component for about page

---
+++ src/components/BannerAbout.js
@@ -0,0 +1,13 @@
+// src/components/BannerAbout.js
+import React from 'react';
+import SecondaryBanner from '../assets/SecondaryBanner.png';
+
+function BannerAbout() {
+    return (
+        <div>
+            <img src={SecondaryBanner} alt="secondary banner"/>
+        </div>
+    );
+}
+
+export default BannerAbout;
