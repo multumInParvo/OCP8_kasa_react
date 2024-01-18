75b0ce8023cad9f7649b5743cf4824c240c3a84b
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-18 03:44:26 +0100

    added banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+// src/components/Banner.js
+import React from 'react';
+import MainBanner from '../assets/MainBanner.png';
+
+function Banner() {
+    return (
+        <div>
+            <img src={MainBanner} alt="main banner"/>
+        </div>
+    );
+}
+
+export default Banner;
