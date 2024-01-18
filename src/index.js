a27770ed4e0d6f74f4b08a21aa63219582f16022
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-18 03:50:27 +0100

    removed unnecessary elements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Change the import
+import { createRoot } from 'react-dom/client'; 
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById('root')); // Use createRoot instead of ReactDOM.createRoot
+const root = createRoot(document.getElementById('root')); 
 root.render(
   <React.StrictMode>
     <Router>
@@ -14,4 +13,4 @@ root.render(
   </React.StrictMode>,
 );
 
-reportWebVitals();
+
