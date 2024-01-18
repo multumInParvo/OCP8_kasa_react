eaab59b2845b65e1106da95e559a259a481e4fd9
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-18 12:12:24 +0100

    added secondary banner component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,11 @@
 // src/pages/AboutPage.js
 import React from 'react';
+import BannerAbout from '../components/BannerAbout';
 
 function AboutPage() {
   return (
     <div>
-
+      <BannerAbout />
     </div>
   );
 }
