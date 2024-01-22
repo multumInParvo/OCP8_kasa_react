bc0d8a1614e980903c67c243f1a72e583d28e8dc
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-22 15:47:50 +0100

    file renamed

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+// src/pages/404.js
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <div>
+      <h2>404 - Page Not Found</h2>
+    </div>
+  );
+}
+
+export default NotFoundPage;
