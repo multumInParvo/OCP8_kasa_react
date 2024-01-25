442d8d49eaea654f393a7676c9654e697679baf3
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-25 14:27:25 +0100

    added Collapse component

---
+++ src/components/Collapse.js
@@ -0,0 +1,27 @@
+// src/components/Collapse.js
+import React, { useState } from 'react';
+import '../styles/Collapse.css';
+
+const Collapse = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse-item">
+      <div className="collapse-header" onClick={toggleCollapse}>
+        <div className="collapse-title">{title}</div>
+        <div className={`collapse-icon ${isOpen ? 'open' : ''}`}>
+          <i className="fas fa-chevron-down"></i>
+        </div>
+      </div>
+      <div className={`collapse-content ${isOpen ? 'show' : ''}`}>
+        <div className="collapse-description">{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Collapse;
