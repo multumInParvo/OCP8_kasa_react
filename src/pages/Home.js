7dfb0db011d19d98f2d0c1bb33b38a3918e3af25
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-25 04:13:13 +0100

    updated code with card component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 // src/pages/Home.js
 import React from 'react';
-import Banner from '../components/Banner'; 
+import Banner from '../components/Banner';
 import MainBanner from '../assets/MainBanner.png';
+import Card from '../components/Card';
+import AppartmentsData from '../data/appartments.json';
 
-function Home() {
+const Home = () => {
   return (
     <div>
       <Banner imageSrc={MainBanner} altText='main banner' />
+
+      <div className="card-container">
+        {AppartmentsData.map((appartment) => (
+          <Card key={appartment.id} appartment={appartment} />
+        ))}
+      </div>
     </div>
   );
 }
