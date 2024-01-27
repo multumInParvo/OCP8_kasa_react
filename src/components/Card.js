48358a9e3fdafc49bc82ec08b9597ab60b1d848e
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-27 23:32:38 +0100

    added link to housing card component on Home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
 // src/components/Card.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Card.css';
 
 const Card = ({ appartment }) => {
-    const { cover, title } = appartment
+    const { cover, title,id } = appartment
 
     return (
         <div>
+          <Link to={`/housing/${id}`}>
           <img className='card' src={cover} alt={title} />
           <h2 className='card-title'>{title}</h2>
+          </Link>
         </div>
       );
     };
