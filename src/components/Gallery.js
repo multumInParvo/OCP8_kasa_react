5990c54f83f85fdb0cb0ef65c2c9d808f60a96c6
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-27 23:53:01 +0100

    created Gallery component

---
+++ src/components/Gallery.js
@@ -0,0 +1,32 @@
+// Gallery.js
+import React, { useState } from 'react';
+
+const Gallery = ({ pictures }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className="gallery">
+      <button onClick={previousImage}>Previous</button>
+      <img
+        src={pictures[currentImageIndex]}
+        alt={`Image ${currentImageIndex + 1}`}
+        className="gallery-image"
+      />
+      <button onClick={nextImage}>Next</button>
+    </div>
+  );
+};
+
+export default Gallery;
