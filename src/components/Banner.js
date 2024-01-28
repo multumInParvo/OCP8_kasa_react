c5901089b2ec2d7d7f0530b172a38be5cf7fb7ad
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-28 21:56:39 +0100

    passed prop to Banner indicating h2 to be displayed or not

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import '../styles/Banner.css';
 
-function Banner({ imageSrc, altText }) {
+function Banner({ imageSrc, altText, showTitle }) {
     return (
         <div className='banner-container'>
             <img className='banner' src={imageSrc} alt={altText} />
+            {showTitle && <h2 className='banner-title'>Chez vous, partout et ailleurs</h2>}
         </div>
     );
 }
