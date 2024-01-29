08c169a4ee5cd04aaecd0a051d88546d13adaf99
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-29 22:42:11 +0100

    added css to Housing page

--- src/pages/Housing.js
+++ src/pages/Housing.js
@@ -1,26 +1,31 @@
 // src/pages/Housing.js
 import React from 'react';
+import '../styles/Housing.css';
 import AppartmentsData from '../data/appartments.json';
 import { useParams } from 'react-router-dom';
 import Gallery from '../components/Gallery';
 
 const Housing = () => {
-    const { id } = useParams(); 
-    const appartment = AppartmentsData.find(item => item.id === id);
+  const { id } = useParams();
+  const appartment = AppartmentsData.find(item => item.id === id);
 
-    if (!appartment) {
+  if (!appartment) {
 
-        return <div>Appartment not found</div>;
-      }
-    
-    return (
-      <div>
-        <Gallery pictures={appartment.pictures} />
-        <h1>{appartment.title}</h1> 
-        <h2>{appartment.location}</h2> 
-        <p>{appartment.tags}</p>
-      </div>
-    );
+    return <div>Appartment not found</div>;
   }
-  
+
+  return (
+    <div>
+      <Gallery pictures={appartment.pictures} />
+      <h1 className='housing-title'>{appartment.title}</h1>
+      <h2 className='housing-location'>{appartment.location}</h2>
+      <div className='tags-container'>
+        {appartment.tags.map((tag, index) => (
+          <span key={index} className='tag'>{tag}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default Housing;
