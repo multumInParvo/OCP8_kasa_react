74376fd742877cc9ea84e22fc5c158d6f4a8ef08
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-31 20:59:26 +0100

    added active link indicator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 // Header.js
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logoheader.png';
 import '../styles/Header.css';
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header>
       <div>
         <img src={logo} alt="Logo header" />
       </div>
       <nav>
-          <a className='nav-links' href="/">Accueil</a>
-          <a className='nav-links' href="/About">A Propos</a>
+        <Link to="/" className={location.pathname === "/" ? 'nav-links active' : 'nav-links'}>Accueil</Link>
+        <Link to="/about" className={location.pathname === "/about" ? 'nav-links active' : 'nav-links'}>A Propos</Link>
       </nav>
     </header>
   );
