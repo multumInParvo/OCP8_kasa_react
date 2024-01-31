e65f712bd14a455f80b7da241e4ba85600eafdc0
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-01-31 21:08:09 +0100

    imported Link component and indicated path to Home.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 // src/pages/404.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/404.css';
 
 function NotFoundPage() {
@@ -7,7 +8,7 @@ function NotFoundPage() {
     <div className='not-found-container'>
       <h1 className='not-found-title'>404</h1>
       <h2 className='not-found-description'>Oups! La page que vous demandez n'existe pas.</h2>
-      <a className='return-home-button' href='#'>Retourner sur la page d’accueil</a>
+      <Link to="/" className='return-home-button'>Retourner sur la page d’accueil</Link>
     </div>
   );
 }
